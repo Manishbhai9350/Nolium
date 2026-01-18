84360be570de526595528360bcc86f652baf0580
Manishbhai9350 <keshraram294@gmail.com>
2026-01-18 11:30:28 +0530

    Awaiting the TRPC prefetch before dehydrating.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import Client from "./client";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default function Home() {
+export default async function Home() {
 
   const queryClient = getQueryClient()
   void queryClient.prefetchQuery(trpc.getUsers.queryOptions())

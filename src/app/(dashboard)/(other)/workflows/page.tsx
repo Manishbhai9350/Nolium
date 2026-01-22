19618bee4187eb9a8273f2d3f8349d2f8aa114d0
Manishbhai9350 <keshraram294@gmail.com>
2026-01-22 22:25:36 +0530

    09 Payments SDK Polar

--- src/app/(dashboard)/(other)/workflows/page.tsx
+++ src/app/(dashboard)/(other)/workflows/page.tsx
@@ -1,10 +1,28 @@
-import { requireAuth } from '@/lib/auth-utils'
-import React from 'react'
+'use client';
 
-const WorkflowsPage = async () => {
-  await requireAuth()
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+// import { requireAuth } from '@/lib/auth-utils'
+
+const WorkflowsPage = /* async */ () => {
+  const trcp = useTRPC()
+  const testAI = useMutation(trcp.testAi.mutationOptions({
+    onSuccess(){
+      toast.success("Test AI")
+    },
+    onError({ message }) {
+      toast.error(message)
+    },
+  }))
+  // await requireAuth()
   return (
-    <div>Workflows</div>
+    <div className='p-8'>
+      <Button disabled={testAI.isPending} onClick={() => testAI.mutate()}>
+        Test AI
+      </Button>
+    </div>
   )
 }
 

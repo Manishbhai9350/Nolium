27d4e7a5113f68663328839e179dedaad9c66a51
Manishbhai9350 <keshraram294@gmail.com>
2026-02-04 21:21:21 +0530

    28: Ready For Production

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators:false,
+  // typescrip`t: {
+  //   ignoreBuildErrors: true,
+  // },`
   async redirects() {
     return [
       {
